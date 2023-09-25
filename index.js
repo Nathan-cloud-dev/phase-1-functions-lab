ab24761319c135e107b7b64c378ba7cff1db4a03
Nathan <kiprotichnathan@yahoo.com>
2023-09-25 11:05:18 +0300

    task done

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    //HQ definition
+    const headquarters = 42;
+    //calculates number of blocks
+    const blocksAway = Math.abs(headquarters - pickup);
+    return blocksAway;
+
+
+}
+function distanceFromHqInFeet(pickup)
+{
+    const block =distanceFromHqInBlocks(pickup);
+    
+
+const feetPerBlock = 264;
+
+return blocks * feetPerBlock;
+}
+function distanceTravelledInFeet(sBlock, eBlock){
+    const blocks = Math.abs(eBlock - sBlock);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+
+}
+function calculatesFarePrice(start, destination){
+
+    const distance = distanceTravelledInFeet(start, destination)
+if (distance > 2500){
+    return 'cannot travel that far';
+
+}
+else if (distance <= 400){
+    return 0;
+}
+else if (distance > 400 && distance <= 2000){
+    return (distance - 400) * .02;
+}
+else{
+    return 25;
+}
+}
+
+    
